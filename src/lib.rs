7f8a1b03506981819551eae70bf709aa175a88c3
leruaa <aurelien.catinon@gmail.com>
2024-02-14 20:26:53 +0000

    feat: init Flashbots transport

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use alloy_transport_http::Http;
+
+#[derive(Debug, Clone)]
+pub struct Flashbots<T, S> {
+    http: Http<T>,
+    signer: S,
+}
+
+impl<T, S> Flashbots<T, S> {
+    /// Create a new [`Flashbots`] transport.
+    pub fn new(http: Http<T>, signer: S) -> Self {
+        Self { http, signer }
+    }
+}
