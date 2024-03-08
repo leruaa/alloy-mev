f2569675e8bf6ea91d1463e6ff8b6d39daf252b2
leruaa <aurelien.catinon@gmail.com>
2024-03-08 20:01:58 +0000

    Refactor extension trait

--- src/provider_ext.rs
+++ src/provider_ext.rs
@@ -9,8 +9,26 @@ use mev_share::rpc::{
 use crate::http::FlashbotsHttp;
 
 #[async_trait]
-pub trait FlashbotsProviderExt<N, C, S>: Provider<N, FlashbotsHttp<C, S>>
+pub trait FlashbotsProviderExt<N, C, S>
 where
+    N: Network,
+    FlashbotsHttp<C, S>: Transport + Clone + Send + Sync,
+    C: Send + Sync,
+    S: Send + Sync,
+{
+    async fn send_bundle(&self, bundle: SendBundleRequest) -> TransportResult<SendBundleResponse>;
+
+    async fn sim_bundle(
+        &self,
+        bundle: SendBundleRequest,
+        sim_overrides: SimBundleOverrides,
+    ) -> TransportResult<SimBundleResponse>;
+}
+
+#[async_trait]
+impl<T, N, C, S> FlashbotsProviderExt<N, C, S> for T
+where
+    T: Provider<N, FlashbotsHttp<C, S>>,
     N: Network,
     FlashbotsHttp<C, S>: Transport + Clone + Send + Sync,
     C: Send + Sync,
@@ -29,14 +47,4 @@ where
             .prepare("mev_simBundle", (bundle, sim_overrides))
             .await
     }
-}
-
-impl<T, N, C, S> FlashbotsProviderExt<N, C, S> for T
-where
-    T: Provider<N, FlashbotsHttp<C, S>>,
-    N: Network,
-    FlashbotsHttp<C, S>: Transport + Clone + Send + Sync,
-    C: Send + Sync,
-    S: Send + Sync,
-{
 }
