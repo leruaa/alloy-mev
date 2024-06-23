59e5ebc6d04ea1fd44d5a34bd3294027e4ddcc8e
leruaa <aurelien.catinon@gmail.com>
2024-06-23 05:13:58 +0000

    Update to `alloy` v0.1

--- tests/sim.rs
+++ tests/sim.rs
@@ -1,11 +1,11 @@
 use std::env;
 
-use alloy::network::EthereumSigner;
+use alloy::network::EthereumWallet;
 use alloy::primitives::address;
 use alloy::primitives::U256;
 use alloy::providers::{Provider, ProviderBuilder};
 use alloy::rpc::types::eth::TransactionRequest;
-use alloy::signers::wallet::LocalWallet;
+use alloy::signers::local::PrivateKeySigner;
 use alloy_mev::{
     rpc::mev::{Inclusion, SendBundleRequest, SimBundleOverrides},
     MevCapableProviderBuilderExt, MevProviderExt,
@@ -16,20 +16,20 @@ use dotenv::dotenv;
 async fn test_sim_bundle() {
     dotenv().ok();
     let eth_rpc = env::var("ETH_HTTP_RPC").unwrap();
-    let wallet = LocalWallet::random();
-    let signer = EthereumSigner::from(wallet.clone());
+    let signer = PrivateKeySigner::random();
+    let wallet = EthereumWallet::new(signer.clone());
 
     let provider = ProviderBuilder::new()
         .with_recommended_fillers()
-        .signer(signer)
+        .wallet(wallet.clone())
         .with_bundle_managment()
-        .bundle_signer(wallet.clone())
+        .bundle_signer(signer.clone())
         .on_http(eth_rpc.parse().unwrap());
 
     let block_number = provider.get_block_number().await.unwrap();
 
     let tx = TransactionRequest::default()
-        .from(wallet.address())
+        .from(signer.address())
         .to(address!("d8dA6BF26964aF9D7eEd9e03E53415D37aA96045"))
         .value(U256::from(1000000000));
 
